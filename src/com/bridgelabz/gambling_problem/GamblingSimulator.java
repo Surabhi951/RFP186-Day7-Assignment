2801381358f1184113f0391873931f7f21889605
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-20 15:07:26 +0530

    Added: days won and lost

--- src/com/bridgelabz/gambling_problem/GamblingSimulator.java
+++ src/com/bridgelabz/gambling_problem/GamblingSimulator.java
@@ -5,21 +5,41 @@ public class GamblingSimulator {
     static int DAILY_STAKE = 100;
     static int PER_DAY_BET = 1;
     static int win = 1;
+    static int daysWon = 0;
+    static int daysLost = 0;
+    static int totalAmountWonFor20Day = 0;
+    static int totalAmountLostFor20Day = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
-        while(DAILY_STAKE < 150 && DAILY_STAKE > 50){
-            int result = (int)(Math.random()*10) % 2;
-            if (result == win){
-                DAILY_STAKE++;
-                System.out.println("Won");
+        for (int day = 1; day <= 20;day++) {
+            DAILY_STAKE = 100;
+            while (DAILY_STAKE < 150 && DAILY_STAKE > 50) {
+                int result = (int) (Math.random() * 10) % 2;
+                if (result == win) {
+                    DAILY_STAKE++;
+                } else {
+                    DAILY_STAKE--;
+
+                }
             }
-            else{
-                DAILY_STAKE--;
-                System.out.println("lost");
+            if(DAILY_STAKE > 100) {
+                totalAmountWonFor20Day += 50;
+                System.out.println("Won on day "+day + " is "+ (DAILY_STAKE-100));
+                daysWon++;
             }
+            else {
+                totalAmountLostFor20Day -= 50;
+                System.out.println("Lost on day "+day +" is "+ (100-DAILY_STAKE));
+                daysLost++;
+            }
+
         }
-        System.out.println(DAILY_STAKE  );
+
+        System.out.println("Total amount won in 20 days is "+ totalAmountWonFor20Day);
+        System.out.println("Total amount lost in 20 days is "+ totalAmountLostFor20Day);
+        System.out.println("No of days won is: "+ daysWon);
+        System.out.println("No of days Lost is: "+ daysLost);
 
     }
 
