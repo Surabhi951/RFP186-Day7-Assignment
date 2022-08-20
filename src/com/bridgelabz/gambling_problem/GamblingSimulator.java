7506c2df7975556f8f0bef3e11b280b32de5fb02
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-20 13:34:30 +0530

    Added: start with stack and bet every game

--- src/com/bridgelabz/gambling_problem/GamblingSimulator.java
+++ src/com/bridgelabz/gambling_problem/GamblingSimulator.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.gambling_problem;
 
 public class GamblingSimulator {
-    static final int DAILY_STACK = 100;
-    static final int PER_DAY_BET = 1;
+    static int DAILY_STACK = 100;
+    static int PER_DAY_BET = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
